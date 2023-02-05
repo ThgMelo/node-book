29fa7cd345b2d071577201614653e8ed45d33541
ThgMelo <es.thg.melo@gmail.com>
2023-02-05 15:30:35 -0300

    Separando execução de configuração

--- dist/server.js
+++ dist/server.js
@@ -1,11 +1,8 @@
 "use strict";
 
-var _express = _interopRequireDefault(require("express"));
-var _bodyParser = _interopRequireDefault(require("body-parser"));
+var _app = _interopRequireDefault(require("./app"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-const app = (0, _express.default)();
-app.use(_bodyParser.default.json());
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+const port = 3000;
+_app.default.listen(port, () => {
+  console.log(`app running on port ${port}`);
 });
